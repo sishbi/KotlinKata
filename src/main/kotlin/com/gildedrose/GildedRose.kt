a9dde2fb0d6a3e916c617ab9c4a40027c9f63328
Simon <simon@billingsley.me.uk>
2025-08-01 12:40:18 +0100

    Refactor-1

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,72 +1,86 @@
 package com.gildedrose
 
+import com.gildedrose.ext.ItemType.AGED_BRIE
+import com.gildedrose.ext.ItemType.BACKSTAGE_PASSES
+import com.gildedrose.ext.ItemType.CONJURED_MANA_CAKE
+import com.gildedrose.ext.ItemType.SULFURAS_RAGNAROS
+
+private const val MAX_QUALITY = 50
+private const val SELL_IN_11_DAYS = 11
+private const val SELL_IN_6_DAYS = 6
+
 class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        if (items[i].name != "Conjured Mana Cake") {
-                            items[i].quality = items[i].quality - 1
-                        } else {
-                            if (items[i].quality > 1) {
-                                items[i].quality = items[i].quality - 2
-                            } else {
-                                items[i].quality = items[i].quality - 1
-                            }
+        for (item in items) {
+            when (item.name) {
+                SULFURAS_RAGNAROS.itemName -> continue
+                AGED_BRIE.itemName, BACKSTAGE_PASSES.itemName -> {
+                    increaseQualityBeforeSellIn(item)
+                }
+                else -> {
+                    if (item.quality > 0) {
+                        decreaseQuality(item)
+                    }
+                }
+            }
+
+            item.sellIn = item.sellIn - 1
+            if (item.sellIn < 0) {
+                when (item.name) {
+                    AGED_BRIE.itemName, BACKSTAGE_PASSES.itemName -> {
+                        increaseQualityAfterSellIn(item)
+                    }
+                    else -> {
+                        if (item.quality > 0) {
+                            decreaseQuality(item)
                         }
                     }
                 }
+            }
+        }
+    }
+
+    private fun increaseQualityAfterSellIn(item: Item) {
+        when (item.name) {
+            AGED_BRIE.itemName -> {
+                if (item.quality < MAX_QUALITY) {
+                    item.quality += 1
+                }
+            }
+
+            BACKSTAGE_PASSES.itemName -> {
+                item.quality = 0
+            }
+        }
+    }
+
+    private fun increaseQualityBeforeSellIn(item: Item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality = item.quality + 1
+
+            if (item.name == BACKSTAGE_PASSES.itemName) {
+                if (item.sellIn < SELL_IN_11_DAYS && item.quality < MAX_QUALITY) {
+                    item.quality += 1
+                }
+
+                if (item.sellIn < SELL_IN_6_DAYS && item.quality < MAX_QUALITY) {
+                    item.quality += 1
+                }
+            }
+        }
+    }
+
+    private fun decreaseQuality(item: Item) {
+        if (item.name == CONJURED_MANA_CAKE.itemName) {
+            if (item.quality > 1) {
+                item.quality -= 2
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+                item.quality -= 1
             }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                if (items[i].name != "Conjured Mana Cake") {
-                                    items[i].quality = items[i].quality - 1
-                                } else {
-                                    if (items[i].quality > 1) {
-                                        items[i].quality = items[i].quality - 2
-                                    } else {
-                                        items[i].quality = items[i].quality - 1
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+        }
+        else {
+            item.quality -= 1
         }
     }
 
